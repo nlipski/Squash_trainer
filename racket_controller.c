a2732104502d1d91100f9cf909dd90cc86267e93
Nlipski <13ml76@queensu.ca>
2018-11-15 22:24:17 -0500

    Initializing MPU and interrupt handling

--- racket_controller.c
+++ racket_controller.c
@@ -14,6 +14,7 @@ static int num_of_interrupts = 0;
 WiFiClient espClient;
 
 MPU6050 mpu;
+uint8_t devStatus;
 
 void handle_interrupt() {
   interrupt_status++;
@@ -23,15 +24,47 @@ void handle_interrupt() {
 void setup() {
   Serial.begin(115200);
   pinMode(interrupt_pin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interrupt_pin),handle_interrupt, FALLING);
-  mpu.initialize();
-//  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)){
-//    println("Connecting MPU6050");
-//    delay(500);
-//  }
-//  
-//  mpu.calibrateGyro();
-//  mpu.setThreshold(3);
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin),handle_interrupt, RISING);
+  
+
+  devStatus = mpu.dmpInitialize();
+  
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); 
+
+
+
+   // make sure it worked (returns 0 if so)
+if (devStatus == 0) {
+	// turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+	// enable Arduino interrupt detection
+	Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+
+    Serial.println(F(")..."));
+
+    mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
 }
 
 void loop() {
