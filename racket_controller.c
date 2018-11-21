9879260f270f18eac5804ddf2ae60783eec9c724
Nlipski <13ml76@queensu.ca>
2018-11-21 13:03:56 -0500

    mpu initialization

--- racket_controller.c
+++ racket_controller.c
@@ -3,6 +3,24 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include <ESP8266WiFi.h>
 
+struct offsets{
+	int ax_offset;
+	int ay_offset;
+	int az_offset;
+	int gx_offset;
+	int gy_offset;
+	int gz_offset;
+};
+
+struct means{
+	long mean_ax;
+	long mean_ay;
+	long mean_az;
+	long mean_gx;
+	long mean_gy;
+	long mean_gz;
+};
+
 // Pin definition 
 const byte interrupt_pin = 15; // pin D8
 
@@ -15,7 +33,8 @@ byte mpuIntStatus = 0;
 MPU6050 mpu;
 byte devStatus;
 VectorInt16 aa;  
-
+bool dmpReady;
+uint16_t packetSize;
 
 //ESP Wifi initialization
 WiFiClient espClient;
@@ -24,9 +43,12 @@ const IPAddress outIp(192, 168, 1, 11);
 const unsigned int outPort = 9999;
 
 
+struct offsets = {0};
+struct means = {0};
+
+
 void reset_offsets()
 {
-  // Zeroing Gyro and Arduino values
   mpu.setXGyroOffset(0);
   mpu.setYGyroOffset(0);
   mpu.setZGyroOffset(0);
@@ -37,11 +59,41 @@ void reset_offsets()
 
 void supplyOffsets()
 {
-  //supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788);
+  mpu.setXGyroOffset(offsets.gx_offset);
+  mpu.setYGyroOffset(offsets.gy_offset);
+  mpu.setZGyroOffset(offsets.gz_offset);
+  mpu.setXAccelOffset(offsets.ax_offset);
+  mpu.setYAccelOffset(offsets.ay_offset);
+  mpu.setZAccelOffset(offsets.az_offset);
+}
+
+void meansensors(){
+  int i = 0;
+  long buff_ax = 0, buff_ay = 0, buff_az = 0, buff_gx = 0, buff_gy = 0, buff_gz = 0;
+
+  for(i = 0; i < (buffersize+101); i++) {
+    
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    if (i>100 && i<= (buffersize+100)) { 
+      buff_ax += ax;
+      buff_ay += ay;
+      buff_az += az;
+
+      buff_gx += gx;
+      buff_gy += gy;
+      buff_gz += gz;
+    }
+
+    delay(2);
+  }
+  
+  means.mean_ax= buff_ax/buffersize;
+  means.mean_ay= buff_ay/buffersize;
+  means.mean_az= buff_az/buffersize;
+  means.mean_gx= buff_gx/buffersize;
+  means.mean_gy= buff_gy/buffersize;
+  means.mean_gz= buff_gz/buffersize;
 }
 
 
@@ -50,34 +102,48 @@ void handle_interrupt() {
   num_of_interrupts++; 
 }
 
-
-
 bool setupInterrupt()
 {
   pinMode(interrupt_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interrupt_pin),handle_interrupt, RISING);
   return mpu.getIntStatus();
 }
+
 void setup_mpu()
 {
-	
+	Wire.begin();
+	Wire.setClock(400000);
+
+	mpu.initialize();
+
+	reset_offsets();
+
+	devStatus = ;
+
+	if (0 != (devStatus = mpu.dmpInitialize()))
+	{
+		Serial.println("DMP Initialization FAILURE with code: ");
+		Serial.print(devStatus);
+		return;
+	}
+
+	mpu.setDMPEnabled(true);
+	mpu.dmpInitialize();
+	mpuIntStatus = setupInterrupt();
+	dmpReady = true;
+	packetSize = mpu.dmpGetFIFOPacketSize();
+	Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+}
+
+void clear_buffer()
+{
+	while (Serial.available() && Serial.read());
 }
 
 void setup() {
 
   Serial.begin(115200);
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-  //packetSize = mpu.dmpGetFIFOPacketSize();
-
-  devStatus = mpu.dmpInitialize();
-  if (!devStatus) 
-  {
-  	//enable DMP
-  	mpu.setDMPEnabled(true);
-  	mpuIntStatus = setupInterrupt();
-
-  }
 }
 
 void loop() {
